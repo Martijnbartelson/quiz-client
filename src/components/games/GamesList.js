0f496250c0ae9677e8bde3a605b7cca826b65711
anton-mladenov <tonkata@mail.com>
2018-07-11 13:05:29 +0200

    pushing to Master

--- src/components/games/GamesList.js
+++ src/components/games/GamesList.js
@@ -2,83 +2,123 @@ import React, {PureComponent} from 'react'
 import {getGames, createGame} from '../../actions/games'
 import {getUsers} from '../../actions/users'
 import {connect} from 'react-redux'
-import {Redirect} from 'react-router-dom'
-import Button from 'material-ui/Button'
+// import Button from 'material-ui/Button'
 import Paper from 'material-ui/Paper'
 import Card, { CardActions, CardContent } from 'material-ui/Card'
 import Typography from 'material-ui/Typography'
 import './GamesList.css'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import GameDetails from "./GameDetails"
+import { withRouter } from 'react-router-dom'
+
 
 class GamesList extends PureComponent {
-  componentWillMount() {
-    if (this.props.authenticated) {
-      if (this.props.games === null) this.props.getGames()
-      if (this.props.users === null) this.props.getUsers()
-    }
-  }
 
-  renderGame = (game) => {
-    const {users, history} = this.props
+	Button = withRouter(({ history }) => (
+		<button
+		type='button'
+		onClick={ () => { history.push('/games/${game.id}') }}
+		>
+		Join A Game
+		</button>
+	))
 
-    return (<Card key={game.id} className="game-card">
-      <CardContent>
-        <Typography color="textSecondary">
-          This game is played by&nbsp;
-          {/* {
-            game.players
-              .map(player => users[player.userId].firstName)
-              .join(' and ')
-          } */}
-        </Typography>
-        <Typography variant="headline" component="h2">
-          Game #{game.id}
-        </Typography>
-        <Typography color="textSecondary">
-          Status: {game.status}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button
-          size="small"
-          onClick={() => history.push(`/games/${game.id}`)}
-        >
-          Watch
-        </Button>
-      </CardActions>
-    </Card>)
-  }
+	componentWillMount() {
+		if (this.props.authenticated) {
+		if (this.props.games === null) this.props.getGames()
+		if (this.props.users === null) this.props.getUsers()
+		if (this.props.gamez === null) this.props.getGames()
+		}
+	}
 
-  render() {
-    const {games, users, authenticated, createGame} = this.props
+	// componentDidMount() {
+	// 		console.log("props.games: " + Object.values(this.props.gamezzz))
+	// }
 
-    if (!authenticated) return (
-			<Redirect to="/login" />
+	renderGame = (game) => {
+		const {users, history} = this.props
+
+		return (<Card key={game.id} className="game-card">
+		<CardContent>
+			<Typography color="textSecondary">
+			This game is played by&nbsp;
+			{/* {
+				game.players
+				.map(player => users[player.userId].firstName)
+				.join(' and ')
+			} */}
+			</Typography>
+			<Typography variant="headline" component="h2">
+			Game #{game.id}
+			</Typography>
+			<Typography color="textSecondary">
+			Status: {game.status}
+			</Typography>
+		</CardContent>
+		{/* <CardActions>
+			<Button
+			size="small"
+			onClick={() => history.push(`/games/${game.id}`)}
+			>
+			Watch
+			</Button>
+		</CardActions> */}
+		<Router>
+		<CardActions>
+			
+			<Button>
+			{/* <Route exact path="/" component={GameDetails} render={ () => <Redirect to={`/games/${game.id}`} /> } /> */}
+			{/* <Route exact path="/games" component={GameDetails} /> */}
+			Join A Game
+			</Button>
+
+		</CardActions>
+		</Router>
+		</Card>)
+	}
+
+		// import { withRouter } from 'react-router-dom'
+		// // this also works with react-router-native
+
+		// const Button = withRouter(({ history }) => (
+		// <button
+		// type='button'
+		// onClick={() => { history.push('/new-location') }}
+		// >
+		// Click Me!
+		// </button>
+		// ))
+
+	render() {
+		const {games, users, authenticated, createGame} = this.props
+
+		if (!authenticated) return (
+				<Redirect to="/login" />
 		)
 
-    if (games === null || users === null) return null
+		if (games === null || users === null) return null
 
-    return (<Paper className="outer-paper">
-      <Button
-        color="primary"
-        variant="raised"
-        onClick={createGame}
-        className="create-game"
-      >
-        Create Game
-      </Button>
+		return (<Paper className="outer-paper">
+		<Button
+			color="primary"
+			variant="raised"
+			onClick={createGame}
+			className="create-game"
+		>
+			Create Game
+		</Button>
 
-      <div>
-        {games.map(game => this.renderGame(game))}
-      </div>
-    </Paper>)
+		<div>
+			{games.map(game => this.renderGame(game))}
+		</div>
+		</Paper>)
   }
 }
 
 const mapStateToProps = state => ({
   authenticated: state.currentUser !== null,
   users: state.users === null ? null : state.users,
-  games: state.games === null ?
-    null : Object.values(state.games).sort((a, b) => b.id - a.id)
+  games: state.games === null ? null : Object.values(state.games).sort((a, b) => b.id - a.id)
 })
 
 export default connect(mapStateToProps, {getGames, getUsers, createGame})(GamesList)
