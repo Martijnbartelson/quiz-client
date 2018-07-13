6273cdc78373586c260728a645ed4ae24d3542e3
anton-mladenov <tonkata@mail.com>
2018-07-13 09:46:53 +0200

    removed Redux DevTools - quick fix

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 // export const baseUrl = 'http://localhost:4000'
-export const baseUrl = 'https://quiz-awesomeness.herokuapp.com/'
+export const baseUrl = 'https://quiz-awesomeness.herokuapp.com'
 
 export const localStorageJwtKey = 'currentUserJwt'
